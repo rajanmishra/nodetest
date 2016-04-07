98fb23d3a72a7840e05a3e90d290789b506e71e2
rajanmishra2010 <rajanmishra2010@hotmail.com>
2016-04-07 16:25:50 +0530

    updating final changes

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var app = express();
 
+
+
+
+var fs = require('fs'); // this engine requires the fs module
+app.engine('html', function (filePath, options, callback) { // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));
+    // this is an extremely simple template engine
+    var rendered = content.toString().replace('#title#', ''+ options.title +'')
+    .replace('#message#', ''+ options.message +'');
+    return callback(null, rendered);
+  });
+});
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'html'); // register the template engine
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.all('/secret', function (req, res, next) {
-  console.log('Accessing the secret section ...');
-  next(); // pass control to the next handler
-});
 
 app.get('/about', function (req, res) {
- res.download('unzip.php')
+  res.render('about', { title: 'Hey', message: 'Hello there!'});
 });
+
+
+var birds = require('./birds');
+app.use('/birds', birds);
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
